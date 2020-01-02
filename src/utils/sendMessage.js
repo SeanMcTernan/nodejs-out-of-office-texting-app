478681154f392cc75f35f260b6da57c8dd459036
Sean Mc Ternan <sean@beyondthesummitpdd.com>
2020-01-02 15:42:01 -0700

    Update API call to TF stand URL

--- src/utils/sendMessage.js
+++ src/utils/sendMessage.js
@@ -3,7 +3,7 @@ const apiKey = process.env.APIKEY;
 
 const sendMessage = (number, message) => {
     request.post(
-          `https://app2.simpletexting.com/v1/send?token=${apiKey}&phone=${number}&message=${message}`
+          `https://tollfree.simpletexting.com/v1/send?token=${apiKey}&phone=${number}&message=${message}`
         );
 }
 
